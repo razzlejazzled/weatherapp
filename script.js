a47b1cbf567a02ab6aae03db32061c774e2ed26e
RazzleJazzled <robertjgassner@gmail.com>
2020-08-16 14:48:38 -0700

    api displays imperial units

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(".btn-search").on("click", function (e) {
     e.preventDefault();
     let searchTerm = $("#searchTerm").val();
 
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=6e386d169b465bbe84362a63b6f2a0b8"
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&units=imperial&appid=6e386d169b465bbe84362a63b6f2a0b8"
 
     $.ajax({
         url: queryURL,
