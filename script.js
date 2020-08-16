afbbdfcc5e218f71318b561a891f242c7524ea0d
RazzleJazzled <robertjgassner@gmail.com>
2020-08-16 14:30:31 -0700

    setting up ajax

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".btn-search").on("click", function (e) {
+    e.preventDefault();
+    let searchTerm = $("#searchTerm").val();
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&6e386d169b465bbe84362a63b6f2a0b8"
+
+    $ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function (response){
+        console.log(response)
+    })
+})
